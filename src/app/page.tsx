4aa492ac7e5f1ef3959536b2d727184717ebbc14
geleeroyale <krassvs@gmail.com>
2025-09-15 22:55:09 +0200

    better layout when tournaments are disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         </div>
 
         {/* Navigation Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-5xl mx-auto">
+        <div className={`grid grid-cols-1 md:grid-cols-2 ${tournamentsEnabled ? 'lg:grid-cols-3' : ''} gap-8 max-w-5xl mx-auto`}>
           {/* Offline Play */}
           <Link
             href="/play"
