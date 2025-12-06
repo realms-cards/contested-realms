42e39e0a4f011f92eedaa56a969f325f41b23a7c
geleeroyale <krassvs@gmail.com>
2025-12-06 03:29:45 +0100

    rainbow migration

--- prisma/migrations/20251206021500_make_rarity_nullable/migration.sql
+++ prisma/migrations/20251206021500_make_rarity_nullable/migration.sql
@@ -1,3 +1,6 @@
 -- AlterTable: Make rarity nullable on CardSetMetadata
 -- This allows cards without a defined rarity (e.g., promo cards) to be ingested
 ALTER TABLE "public"."CardSetMetadata" ALTER COLUMN "rarity" DROP NOT NULL;
+
+-- AlterEnum: Add Rainbow finish for Gothic set foils
+ALTER TYPE "public"."Finish" ADD VALUE IF NOT EXISTS 'Rainbow';
